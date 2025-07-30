8463d2f71f422a611ec8fa152603ef463fb90324
kashishnigam <42498954+kashishnigam@users.noreply.github.com>
2025-07-30 12:41:50 +0530

    Secrets app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+# os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+HF_TOKEN = st.secrets.get("HF_TOKEN")
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
